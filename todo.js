01f56da16ff29dffef78b6937b6218024aef86b1
ljyockey <leoyockey@gmail.com>
2019-04-27 16:07:30 -0700

    listener setup

--- todo.js
+++ todo.js
@@ -1,2 +1,14 @@
 'use strict';
 console.log('hello, world!');
+
+const deleteItem = (e) => {
+    console.log(e)
+}
+
+const setupListeners = () => {
+    for (let i=0; i < document.getElementsByClassName('delete').length; i++) {
+        document.getElementsByClassName('delete')[i].addEventListener('click', deleteItem)
+    }
+}
+
+setupListeners()
