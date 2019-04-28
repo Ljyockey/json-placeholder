5f83c64dc663e7993b5d56b973fc7d4a66235c3e
ljyockey <leoyockey@gmail.com>
2019-04-28 00:48:35 -0700

    generating list from api data

--- todo.js
+++ todo.js
@@ -7,9 +7,44 @@ const deleteItem = (e) => {
 };
 
 const setupListeners = () => {
-  for (let i=0; i < document.getElementsByClassName('delete').length; i++) {
-    document.getElementsByClassName('delete')[i].addEventListener('click', deleteItem);
+  return getToDos().then(liElements => {
+    liElements.forEach((i) => {
+      i.getElementsByClassName('delete')[0].addEventListener('click', deleteItem);
+    });
+  });
+};
+
+const getToDos = () => {
+  const url = 'https://jsonplaceholder.typicode.com/todos/';
+  return fetch(url)
+    .then(response => response.json())
+    .then(toDos => generateToDoList(toDos));
+};
+
+const generateToDoList = toDos => {
+  const liItems = toDos.map(t => {
+    const element = document.createElement('li');
+    element.appendChild(addLiChildElement(t.title));
+    element.appendChild(addLiChildElement());
+    const container = document.getElementById('container');
+    container.appendChild(element);
+    return element;
+  });
+  return liItems;
+};
+
+const addLiChildElement = (spanText) => {
+  let element;
+  if (spanText) {
+    element = document.createElement('span');
+    element.contentEditable = true;
+    element.textContent = spanText;
+  } else {
+    element = document.createElement('button');
+    element.classList.add('delete');
+    element.textContent = 'Delete';
   }
+  return element;
 };
 
 setupListeners();
