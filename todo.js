caf1ea1a05967f33195fe99750a2b14f6036befd
ljyockey <leoyockey@gmail.com>
2019-04-27 23:12:20 -0700

    removing elements on delete

--- todo.js
+++ todo.js
@@ -1,14 +1,15 @@
 'use strict';
-console.log('hello, world!');
 
 const deleteItem = (e) => {
-    console.log(e)
-}
+  const ulContainer = e.target.parentNode.parentNode;
+  const targetLi = e.target.parentNode;
+  ulContainer.removeChild(targetLi);
+};
 
 const setupListeners = () => {
-    for (let i=0; i < document.getElementsByClassName('delete').length; i++) {
-        document.getElementsByClassName('delete')[i].addEventListener('click', deleteItem)
-    }
-}
+  for (let i=0; i < document.getElementsByClassName('delete').length; i++) {
+    document.getElementsByClassName('delete')[i].addEventListener('click', deleteItem);
+  }
+};
 
-setupListeners()
+setupListeners();
